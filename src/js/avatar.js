975482b003ca64c80b2689ce96eb55a1a0831886
sliwey <qlw1009@gmail.com>
2015-07-17 17:41:59 +0800

    Remove 'not mix mode'.

--- src/js/avatar.js
+++ src/js/avatar.js
@@ -6,14 +6,123 @@
         pixelateBtn = doc.getElementById("pixelate"),
         inverseBtn = doc.getElementById("inverse"),
         grayBtn = doc.getElementById("gray"),
-        resetBtn = doc.getElementById("reset"),
-        mix = doc.getElementById("mix"),
+        initBtn = doc.getElementById("init"),
         canvas = doc.getElementById("canvas"),
         ctx = canvas.getContext("2d"),
         isLoaded = false,
-        isMixed = false,
+        renders = {
+            pixelate: false,
+            inverse: false,
+            gray: false
+        },
         oriImageData,
-        imageData;
+        imageData,
+        filters,
+        CSS_SELECT = "select";
+
+    filters = {
+        pixelate: function() {
+            var pixels = imageData.data,
+                rows = 20,
+                cols = 20,
+                cellWidth = imageData.width / cols,
+                cellHeight = imageData.height / rows,
+                i,j,x,y,pos,r,g,b;
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            for (i = 0; i < rows; i++) {
+                for (j = 0; j < cols; j++) {
+                    x = ~~((cellWidth * j) + (cellWidth / 2));
+                    y = ~~((cellHeight * i) + (cellHeight / 2));
+
+                    pos = x * 4 + ~~(y * imageData.width * 4);
+
+                    r = pixels[pos];
+                    g = pixels[pos + 1];
+                    b = pixels[pos + 2];
+
+                    ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+                    ctx.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+                }
+            }
+
+            imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        },
+        inverse: function() {
+            var pixels = imageData.data,
+                length = imageData.width * imageData.height;
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            for (var i = 0; i < length; i++) {
+                pixels[i * 4] = 255 - pixels[i * 4];
+                pixels[i * 4 + 1] = 255 - pixels[i * 4 + 1];
+                pixels[i * 4 + 2] = 255 - pixels[i * 4 + 2];
+            }
+
+            ctx.putImageData(imageData, 0, 0);
+        },
+        gray: function() {
+            var pixels = imageData.data,
+                length = imageData.width * imageData.height,
+                avg;
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            for (var i = 0; i < length; i++) {
+                avg = ~~((pixels[i * 4] + pixels[i * 4 + 1] + pixels[i * 4 + 2]) / 3);
+                pixels[i * 4] = avg;
+                pixels[i * 4 + 1] = avg;
+                pixels[i * 4 + 2] = avg;
+            }
+
+            ctx.putImageData(imageData, 0, 0);
+        }
+    };
+
+    function init() {
+        reset();
+
+        for (var key in renders) {
+            if (renders.hasOwnProperty(key)) {
+                renders[key] = false;
+            }
+        }
+
+        pixelateBtn.classList.remove(CSS_SELECT);
+        inverseBtn.classList.remove(CSS_SELECT);
+        grayBtn.classList.remove(CSS_SELECT);
+    }
+
+    function reset() {
+        if (oriImageData) {
+            ctx.putImageData(oriImageData, 0, 0);
+            imageData = createImageData(oriImageData);
+        }
+    }
+
+    function render() {
+        reset();
+
+        for (var key in renders) {
+            if (renders.hasOwnProperty(key) && renders[key]) {
+                filters[key]();
+            }
+        }
+    }
+
+    function createImageData(imageData) {
+        var newImageData = ctx.createImageData(imageData.width, imageData.height),
+            pixels = imageData.data,
+            newPixels = newImageData.data;
+
+        for (var i = 0; i < pixels.length; i++) {
+            newPixels[i] = pixels[i]
+        }
+
+        return newImageData;
+    }
 
     function load(file) {
         var image = new Image();
@@ -31,93 +140,6 @@
             imageData = createImageData(oriImageData);
             win.URL.revokeObjectURL(file);
         }
-    }
-
-    function pixelate(imageData, isMixed) {
-        var pixels = imageData.data,
-            rows = 20,
-            cols = 20,
-            cellWidth = imageData.width / cols,
-            cellHeight = imageData.height / rows,
-            i,j,x,y,pos,r,g,b;
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        for (i = 0; i < rows; i++) {
-            for (j = 0; j < cols; j++) {
-                x = ~~((cellWidth * j) + (cellWidth / 2));
-                y = ~~((cellHeight * i) + (cellHeight / 2));
-
-                pos = x * 4 + ~~(y * imageData.width * 4);
-
-                r = pixels[pos];
-                g = pixels[pos + 1];
-                b = pixels[pos + 2];
-
-                ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-                ctx.fillRect(cellWidth * j, cellHeight * i, cellWidth, cellHeight);
-            }
-        }
-
-        if (isMixed) {
-            return ctx.getImageData(0, 0, canvas.width, canvas.height);
-        } else {
-            return imageData;
-        }
-    }
-
-    function inverse(imageData, isMixed) {
-        var image = isMixed ? imageData : createImageData(imageData),
-            pixels = image.data,
-            length = image.width * image.height;
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        for (var i = 0; i < length; i++) {
-            pixels[i * 4] = 255 - pixels[i * 4];
-            pixels[i * 4 + 1] = 255 - pixels[i * 4 + 1];
-            pixels[i * 4 + 2] = 255 - pixels[i * 4 + 2];
-
-        }
-
-        ctx.putImageData(image, 0, 0);
-    }
-
-    function gray(imageData, isMixed) {
-        var image = isMixed ? imageData : createImageData(imageData),
-            pixels = image.data,
-            length = image.width * image.height,
-            avg;
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        for (var i = 0; i < length; i++) {
-            avg = ~~((pixels[i * 4] + pixels[i * 4 + 1] + pixels[i * 4 + 2]) / 3);
-            pixels[i * 4] = avg;
-            pixels[i * 4 + 1] = avg;
-            pixels[i * 4 + 2] = avg;
-        }
-
-        ctx.putImageData(image, 0, 0);
-    }
-
-    function reset() {
-        if (oriImageData) {
-            ctx.putImageData(oriImageData, 0, 0);
-            imageData = createImageData(oriImageData);
-        }
-    }
-
-    function createImageData(imageData) {
-        var newImageData = ctx.createImageData(imageData.width, imageData.height),
-            pixels = imageData.data,
-            newPixels = newImageData.data;
-
-        for (var i = 0; i < pixels.length; i++) {
-            newPixels[i] = pixels[i]
-        }
-
-        return newImageData;
     }
 
 
@@ -159,29 +181,42 @@
 
     pixelateBtn.addEventListener("click", function() {
         if (isLoaded) {
-            imageData = pixelate(imageData, isMixed);
+            renders.pixelate = !renders.pixelate;
+            if (renders.pixelate) {
+                this.classList.add(CSS_SELECT);
+            } else {
+                this.classList.remove(CSS_SELECT);
+            }
+            render();
         }
     }, false);
 
     inverseBtn.addEventListener("click", function() {
         if (isLoaded) {
-            inverse(imageData, isMixed);
+            renders.inverse = !renders.inverse;
+            if (renders.inverse) {
+                this.classList.add(CSS_SELECT);
+            } else {
+                this.classList.remove(CSS_SELECT);
+            }
+            render();
         }
     }, false);
 
     grayBtn.addEventListener("click", function() {
         if (isLoaded) {
-            gray(imageData, isMixed);
+            renders.gray = !renders.gray;
+            if (renders.gray) {
+                this.classList.add(CSS_SELECT);
+            } else {
+                this.classList.remove(CSS_SELECT);
+            }
+            render();
         }
     }, false);
 
-    resetBtn.addEventListener("click", function() {
-        reset();
-    }, false);
-
-    mix.addEventListener("change", function() {
-        isMixed = this.checked;
-        reset();
+    initBtn.addEventListener("click", function() {
+        init();
     }, false);
 
 })(document, window);
